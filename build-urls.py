e1297ecdd195806c6bf404ea1c6f9d3669960f41
Rudi Niemeijer <rudiniemeijer@gmail.com>
2019-02-08 13:08:36 +0100

    Create build-urls.py

---
+++ build-urls.py
@@ -0,0 +1,19 @@
+from flask import Flask, url_for
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return 'indexpagina'
+
+@app.route('/login')
+def login():
+    return 'login'
+
+@app.route('/gebruiker/<gebruikernaam>')
+def profielpagina(gebruikernaam):
+    return '{}\'s profielpagina'.format(gebruikernaam)
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profielpagina', gebruikernaam='Joep Meloen'))
